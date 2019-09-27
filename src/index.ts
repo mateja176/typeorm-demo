9cb217284d8fd33b68dbea43ba99b1602ca25147
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-27 22:01:41 +0200

    simplifies index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
+import chalk from 'chalk';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import { createApp } from './app';
 import mongoConfig from './config/mongo';
 
+const port = process.env.PORT || 3000;
+
 createConnection(mongoConfig)
-  .then(async connection => {
-    const app = createApp(connection);
-
-    const port = process.env.PORT || 3000;
-
-    app.listen(port, () => {
+  .then(connection => {
+    createApp(connection).listen(port, () => {
       console.log(`http://localhost:${port}`);
     });
   })
-  .catch(error => console.log(error));
+  .catch(error => console.log(chalk.magenta(error)));
