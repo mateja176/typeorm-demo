ba75ed087a2dd5d0fae24e4c0c77553e1f9de35a
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-28 14:42:17 +0200

    adds ArmySubscriber

---
+++ src/subscriber/ArmySubscriber.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+import {
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+} from 'typeorm';
+import { Army } from '../entity/Army';
+
+@EventSubscriber()
+export class ArmySubscriber implements EntitySubscriberInterface<Army> {
+  listenTo() {
+    return Army;
+  }
+
+  beforeInsert(event: InsertEvent<Army>) {
+    console.log(event);
+  }
+}
