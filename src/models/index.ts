97d7a09e854f95f28fd942555f88432e8d5985d6
Mateja Petrovic <ma.petrovic@levi9.com>
2019-09-28 12:49:40 +0200

    expands request with user

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,9 @@
 import { Request as ExpressRequest } from 'express';
 import { Connection } from 'typeorm';
+import { Army, ArmyDto } from '../entity/Army';
 
-export interface Request extends ExpressRequest {
+export interface Request<User extends Army | ArmyDto | undefined = undefined>
+  extends ExpressRequest {
   db: Connection;
+  user: User;
 }
